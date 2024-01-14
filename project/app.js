5d6241e37f6b361e3355f5159c7f2bbcef16d589
dibkb <dibas9110@gmail.com>
2024-01-14 15:27:51 +0530

    event emitter on app

--- project/app.js
+++ project/app.js
@@ -1,10 +1,30 @@
 const fs = require("fs/promises");
 (async () => {
+  const commandFileHandler = await fs.open("./command.txt", "r");
+
+  commandFileHandler.on("change", async () => {
+    //   get size of the file
+    const size = (await commandFileHandler.stat()).size;
+    const buff = Buffer.alloc(size);
+    // location at which to start filling our buffer
+    const offset = 0;
+    // bytes we want to read
+    const length = buff.byteLength;
+    // positon that we want to start reading the file from
+    const position = 0;
+    // alll the content
+    const content = await commandFileHandler.read(
+      buff,
+      offset,
+      length,
+      position
+    );
+    console.log(content);
+  });
   const watcher = fs.watch("./");
   for await (const event of watcher) {
     if (event.eventType === "change" && event.filename === "command.txt") {
-      // the file was changed...
-      console.log("The file was changed");
+      commandFileHandler.emit("change");
     }
   }
 })();
